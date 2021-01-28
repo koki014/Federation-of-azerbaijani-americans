94d1fc286588ac9ca8aa5d63f37452a891e30bf0
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2021-01-28 15:45:17 +0400

    superuser added

--- account/models.py
+++ account/models.py
@@ -9,18 +9,18 @@ class User(AbstractUser):
     username = None
     last_name = None
     first_name = None
-    full_name = models.CharField(_('full name'), max_length=80, blank=False, null=False )
+    full_name = models.CharField(_('full name'), max_length=80 )
     email = models.EmailField(_('email address'), unique=True)
     birthday=models.DateField(auto_now=False, null=True, blank=True)
-    phone_number = models.CharField(_('phone number'), max_length=30, blank=False, null=False)
-    citizenship = models.CharField(_('citizenship'), max_length=225, blank=False, null=False )
-    education = models.CharField(_('education'), max_length=225, blank=False, null=False)
-    current = models.CharField(_('current'), max_length=225, blank=False, null=False)
-    permoment = models.CharField(_('permoment'), max_length=225, blank=False, null=False)
-    memeber_of_ngo = models.CharField(_('memeber of ngo'), max_length=225, blank=False, null=False)
-    usa_year = models.IntegerField(_('usa year'), blank=False, null=False)
-    reasons = models.TextField(_('reasons'), blank=False, null=False)
-    mention = models.TextField(_('mention'), blank=False, null=False)
+    phone_number = models.CharField(_('phone number'), max_length=30)
+    citizenship = models.CharField(_('citizenship'), max_length=225 )
+    education = models.CharField(_('education'), max_length=225)
+    current = models.CharField(_('current'), max_length=225)
+    permoment = models.CharField(_('permoment'), max_length=225)
+    memeber_of_ngo = models.CharField(_('memeber of ngo'), max_length=225)
+    usa_year = models.IntegerField(_('usa year'), null=True)
+    reasons = models.TextField(_('reasons'))
+    mention = models.TextField(_('mention'))
 
 
     USERNAME_FIELD = 'email'
--- ffa/settings.py
+++ ffa/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account',
-    'core'
+    'account.apps.AccountConfig',
+    'core.apps.CoreConfig'
 
 ]
 
@@ -77,10 +77,18 @@ WSGI_APPLICATION = 'ffa.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'ffa',
+            'USER': 'user_ffa',
+            'PASSWORD': '12345',
+            'PORT': '5432',
+            'HOST': '127.0.0.1'
+        }
 }
 
 
