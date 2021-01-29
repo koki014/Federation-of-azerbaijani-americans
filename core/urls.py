f0f67d4841ca38a24cea93f3dd103e2493c87986
Elvin <59554164+husubayli-elvin@users.noreply.github.com>
2021-01-29 16:29:11 +0400

    fixed contact form and subscriber form with one view

--- core/urls.py
+++ core/urls.py
@@ -1,18 +1,14 @@
 from django.urls import path
 
 from .views import (
-    ContactView,
-    SubscriberView
+    HomePageView
 )
 
 app_name = 'core'
 
 
 urlpatterns = [
-<<<<<<< HEAD
     path("", HomePageView.as_view(), name="index")
-=======
-    path("", ContactView.as_view(), name="index"),
-    path("", SubscriberView.as_view(), name='index')
->>>>>>> 118a1708e4ad598fe1bf563510c32302a2c955fa
+    # path("", ContactView.as_view(), name="index"),
+    # path("", SubscriberView.as_view(), name='index')
 ]
--- core/views.py
+++ core/views.py
@@ -1,20 +1,59 @@
 from core.models import Contact
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import (
-    CreateView
+    CreateView,
+    TemplateView,
 )
+from django.views import View
 from django.urls.base import reverse_lazy
 
 from .forms import ContactForm, SubscriberForm
 
 
-class ContactView(CreateView):
-    form_class = ContactForm
-    template_name = 'index.html'
-    success_url = reverse_lazy('core:index')
+# class ContactView(CreateView):
+#     form_class = ContactForm
+#     template_name = 'index.html'
+#     success_url = reverse_lazy('core:index')
 
 
-class SubscriberView(CreateView):
-    form_class = SubscriberForm
-    template_name = 'index.html'
-    success_url = reverse_lazy('core:index')
+# class SubscriberView(CreateView):
+#     form_class = SubscriberForm
+#     template_name = 'index.html'
+#     success_url = reverse_lazy('core:index')
+
+# class HomePageView(TemplateView):
+#     template_name = 'index.html'
+
+
+class HomePageView(View):
+
+    def get(self, request):
+        contact_form = ContactForm(prefix='contact_form')
+        subscribers_form = SubscriberForm(prefix='subscribers_form')
+        context = {
+            'contact_form': contact_form,
+            'subscribers_form': subscribers_form
+        }
+        return render(request, 'index.html', context)
+
+    def post(self, request):
+        contact_form = ContactForm(prefix='contact_form')
+        subscribers_form = SubscriberForm(prefix='subscribers_form')
+        
+        action = self.request.POST['action']
+
+        if action == 'contact_us':
+            contact_form = ContactForm(request.POST, prefix='contact_form')
+            if contact_form.is_valid():
+                form.save()
+                return redirect('')
+        elif action == 'subscribe':
+            contact_form = SubscriberForm(request.POST, prefix='subscribers_form')
+            if subscribers_form.is_valid():
+                form.save()
+                return redirect('')
+        context = {
+            'contact_form': contact_form,
+            'subscribers_form': subscribers_form
+        }
+        return render(request, 'index.html', context)
