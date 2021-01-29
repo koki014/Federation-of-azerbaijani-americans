15eacaf8aa28b52e24d6d5042b0908d7fac5c6ef
koki014 <koki.suleymanov@gmail.com>
2021-01-29 20:15:12 +0400

    urls

--- account/urls.py
+++ account/urls.py
@@ -9,7 +9,7 @@ app_name = 'account'
 
 urlpatterns = [
     path("register/", RegisterCreateView.as_view(), name="register"),
-    path("register-done/", RegisterDoneView.as_view(), name="register_done"),
+    path("congratulations/", RegisterDoneView.as_view(), name="register_done"),
     path("donante/", DonateView.as_view(), name='donate' )
 
 ]
