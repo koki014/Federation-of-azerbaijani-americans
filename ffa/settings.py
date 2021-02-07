ecf7a8cece53c327e0089a7f203cafd8ae6b27c8
Elvin Husubeyli <husubayli@gmail.com>
2021-02-07 15:18:03 +0400

    nginx deployment

--- ffa/settings.py
+++ ffa/settings.py
@@ -18,16 +18,20 @@ from django.urls import reverse_lazy
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '2v*67*oids=1!=d%#s*)h+7n9zn67fzcc=if-q@)@gbu0uwk4l'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False if os.environ.get('DEBUG') else True
+PROD = not DEBUG
+
+
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -83,21 +87,28 @@ WSGI_APPLICATION = 'ffa.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': {
+if PROD:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': os.environ.get('POSTGRES_HOST'),
+            'PORT': os.environ.get('POSTGRES_PORT')
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'ffa',
             'USER': 'user_ffa',
+            'PORT': 5432,
             'PASSWORD': '12345',
-            'PORT': '5432',
-            'HOST': '127.0.0.1'
+            'HOST': '127.0.0.1',
         }
-}
+    }
 
 
 # Password validation
