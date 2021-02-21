7c2c301c151431dc2b0b8059d68c67d585e4f813
Elvin Husubeyli <husubayli@gmail.com>
2021-02-21 23:45:35 +0400

    ss

--- ffa/settings.py
+++ ffa/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = '/static/'
 if PROD:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 else:
     STATICFILES_DIRS = [
         BASE_DIR / "static",
