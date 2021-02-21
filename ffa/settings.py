00c21c2374b389de26e0991fffcedaa0ba6756b2
Elvin Husubeyli <husubayli@gmail.com>
2021-02-21 22:45:44 +0400

    smtp mail change

--- ffa/settings.py
+++ ffa/settings.py
@@ -161,8 +161,10 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'tech.academy.docker@gmail.com'
-EMAIL_HOST_PASSWORD = 'suqmnhaxezvemyhn'
+EMAIL_HOST_USER = 'husubayli@gmail.com'
+EMAIL_HOST_PASSWORD = 'xdjnasiuddxikfax'
+
+
 
 STATIC_URL = '/static/'
 if PROD:
